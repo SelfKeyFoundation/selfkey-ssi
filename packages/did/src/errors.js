b5c705d5eaa3d9569c29a6915963e1ccf66ba962
Maxim Kovalov <max.kovalov@gmail.com>
2021-02-11 15:27:47 +0200

    feat: add universal resolver client

--- packages/did/src/errors.js
+++ packages/did/src/errors.js
@@ -3,3 +3,10 @@ export class InvalidDidError extends Error {
         super('Invalid DID');
     }
 }
+
+export class ResolutionError extends Error {
+    constructor(code, message) {
+        super(message);
+        this.code = code;
+    }
+}
---
+++ packages/did/src/mime-types.js
@@ -0,0 +1,4 @@
+export const DID_JSON = 'application/did+json';
+export const DID_JSON_LD = 'application/did+ld+json';
+export const DID_CBOR = 'application/did+cbor';
+export const DID_DAG_CBOR = 'application/did+dag+cbor';
